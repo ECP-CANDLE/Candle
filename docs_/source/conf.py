8bf7068843c53196638080e63a316687aaf216d3
Hyunseung Yoo <hsyoo@anl.gov>
2018-10-18 15:52:45 -0500

    turn of return type

--- docs_/source/conf.py
+++ docs_/source/conf.py
@@ -55,6 +55,8 @@ extensions = [
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 napoleon_use_ivar = True
+napoleon_use_rtype = False
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
