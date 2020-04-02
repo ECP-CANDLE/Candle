eb2a82205db0bb78ccba5b923b50b621053eb2c4
Harry Yoo <hsyoo@anl.gov>
2020-04-02 10:12:59 -0500

    add data_preprocessing_utils

--- docs_/source/conf.py
+++ docs_/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath('../../examples/mnist'))
 # -- Project information -----------------------------------------------------
 
 project = 'CANDLE'
-copyright = '2018, ANL.gov'
+copyright = '2020, ANL.gov'
 author = 'CANDLE Team'
 
 # The short X.Y version
